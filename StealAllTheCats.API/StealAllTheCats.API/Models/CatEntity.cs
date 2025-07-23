f7fa5071d7bde1f549f12a27f1c75d46ad909d63
jimisbek <jimisbek@hotmail.gr>
2025-07-23 21:25:19 +0300

    Add Models folder.

---
+++ StealAllTheCats.API/StealAllTheCats.API/Models/CatEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace StealAllTheCats.API.Models
+{
+    public class CatEntity
+    {
+        public int Id { get; set; }
+        public string CatId { get; set; } = string.Empty;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ StealAllTheCats.API/StealAllTheCats.API/Models/TagEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace StealAllTheCats.API.Models
+{
+    public class TagEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+    }
+}
